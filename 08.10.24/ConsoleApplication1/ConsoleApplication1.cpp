2b3eb45b6527d18e179020f9b49cdf216bedf050
Bongo664 <patrykpojda@gmail.com>
2024-10-08 08:49:41 +0200

    a

---
+++ 08.10.24/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+class Zwierze {
+public:
+	Zwierze() { cout << "Zwierze stworzone" << endl; }
+	~Zwierze() { cout << "Zwierze zniszczone" << endl; }
+};
+int main()
+{
+    cout << "konstr, destr\n";
+	Zwierze z;
+    return EXIT_SUCCESS;
+}
---
+++ 08.10.24/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+static class matematyka {
+public:
+	static constexpr double PI = 3.14159;
+	static int silnia(int n) {
+		if (n <= 1) return 1;
+		return n * silnia(n - 1);
+	}
+};
+int main()
+{
+	cout << "\n";
+}
